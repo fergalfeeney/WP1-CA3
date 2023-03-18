53d91c5828d26c6f87bfbc27c6166d0c077d6ef5
fergalfeeney <98536841+fergalfeeney@users.noreply.github.com>
2023-03-18 16:11:53 +0000

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { OmbdApiService } from './services/ombd-api.service';
+import { SearchtitleComponent } from './components/searchtitle/searchtitle.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes:Routes=[
+  {path:'', component:SearchtitleComponent},
+  {path:'about', component:AboutComponent},
+  {path: 'search', component:SearchComponent},
+    ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchtitleComponent,
+    AboutComponent,
+    SearchComponent
   ],
+
+
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     HttpClientModule
   ],
   providers: [OmbdApiService],
